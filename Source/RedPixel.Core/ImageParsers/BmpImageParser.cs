c47bf6395c71368ddff67e17bdb27bed93022a42
bibletoon <alex8.ivanov@gmail.com>
2022-10-18 13:39:33 +0300

    Refactor: bmp parsing

---
+++ Source/RedPixel.Core/ImageParsers/BmpImageParser.cs
@@ -0,0 +1,18 @@
+﻿namespace RedPixel.Core.ImageParsers;
+
+using ImageFormat = System.Drawing.Imaging.ImageFormat;
+public class BmpImageParser : IImageParser
+{
+    public Core.ImageFormat[] ImageFormats => new[] { Core.ImageFormat.Bmp, };
+
+    public Bitmap.Bitmap Parse(Stream content)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SerializeToStream(Bitmap.Bitmap image, Stream stream)
+    {
+        var img = image.GetSystemBitmap();
+        img.Save(stream, ImageFormat.Bmp);
+    }
+}
--- Source/RedPixel.Ui/Utility/ImageExtensions.cs
+++ Source/RedPixel.Ui/Utility/ImageExtensions.cs
@@ -1,9 +1,8 @@
-﻿using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using RedPixel.Core.Bitmap;
+﻿using System.IO;
 using RedPixel.Core.Colors;
+using RedPixel.Core.ImageParsers;
 using Bitmap = Avalonia.Media.Imaging.Bitmap;
+using ImageFormat = RedPixel.Core.ImageFormat;
 using RedPixelBitmap = RedPixel.Core.Bitmap.Bitmap;
 
 namespace RedPixel.Ui.Utility;
@@ -15,7 +14,7 @@ public static class ImageExtensions
         using var ms = new MemoryStream();
         if (components != ColorComponents.All)
             bitmap = bitmap.SelectColorComponents(components);
-        BitmapSaverFactory.CreateSaver(ImageFormat.Bmp).Save(bitmap, ms);
+        ImageParserFactory.CreateParser(ImageFormat.Bmp).SerializeToStream(bitmap, ms);
         ms.Position = 0;
         return new Bitmap(ms);
     }

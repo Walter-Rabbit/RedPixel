60dac77d2980ca351a8ab529468d3909e231620e
Valery Shevchenko <ar4er.valera@gmail.com>
2022-11-09 23:04:00 +0300

    chore: default case

--- Source/RedPixel.Core/Colors/Extensions/RgbGammaConversion.cs
+++ Source/RedPixel.Core/Colors/Extensions/RgbGammaConversion.cs
@@ -4,6 +4,11 @@ public static class RgbGammaConversion
 {
     public static RgbColor AssignGamma(this RgbColor rgbColor, float gammaDiffValue = 0f)
     {
+        if (gammaDiffValue == 0f)
+        {
+            return rgbColor;
+        }
+
         return new RgbColor(
             (float)(255 * Math.Pow(rgbColor.FirstComponent, gammaDiffValue) / Math.Pow(255, gammaDiffValue)),
             (float)(255 * Math.Pow(rgbColor.SecondComponent, gammaDiffValue) / Math.Pow(255, gammaDiffValue)),

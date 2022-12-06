87f96d682d9d3e67dafa7e64f25fb7a2d74accea
Valery Shevchenko <ar4er.valera@gmail.com>
2022-12-06 13:41:12 +0300

    feat: box filter

--- Source/RedPixel.Core/Tools/Filtering/MedianFiltering.cs
+++ Source/RedPixel.Core/Tools/Filtering/MedianFiltering.cs
@@ -8,7 +8,8 @@ public class MedianFiltering : IFiltering
 {
     public static Bitmap ApplyFiltering(Bitmap bitmap, float coreRadius, Point leftTopPoint, Point rightBottomPoint)
     {
-        var capacity = (int)((2 * coreRadius + 1) * (2 * coreRadius + 1));
+        var radius = (int)Math.Round(coreRadius);
+        var capacity = (2 * radius + 1) * (2 * radius + 1);
         var areaPixels = new List<float[]>
         {
             new float[capacity],
@@ -17,8 +18,6 @@ public class MedianFiltering : IFiltering
         };
         var newBitmap = new Bitmap(bitmap.Width, bitmap.Height, bitmap.BytesForColor, bitmap.ColorSpace);
         newBitmap.Matrix = bitmap.Matrix.Clone() as Color[,];
-
-        var radius = (int)Math.Round(coreRadius);
 
         for (var i = leftTopPoint.X; i <= rightBottomPoint.X; i++)
         {
